87cdb1faa7999cacda89e8f995cf28eadacfe7ec
alvaro-andrade <alvaro.andrade@salesforce.com>
2019-10-03 11:36:02 -0300

    Removed country from logic

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,20 +4,23 @@ const API_KEY = "623b80fca82aa938a4dadc11ffe73bd3";
 
 class Form extends React.Component {
   state = {
-    city: "",
-    country: ""
+    city: ""
   };
 
   getWeather = async e => {
     e.preventDefault();
     const weatherData = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${this.state.city},${this.state.country}&appid=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/forecast?q=${this.state.city}&appid=${API_KEY}`
     );
     const jRes = await weatherData.json();
     console.log("response => ", jRes);
-    jRes.cod !== "200"
-      ? this.props.setError({ error: jRes.message })
-      : this.props.setForecast(jRes.list);
+    if (jRes.cod !== "200") {
+      this.props.setError({ error: jRes.message });
+      this.props.setForecast(null);
+    } else {
+      this.props.setError({ error: null });
+      this.props.setForecast(jRes.list);
+    }
   };
 
   handleChange = e => {
@@ -36,14 +39,6 @@ class Form extends React.Component {
           id="city"
           placeholder="Enter city"
           value={this.state.city}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="country"
-          id="country"
-          placeholder="Enter country"
-          value={this.state.country}
           onChange={this.handleChange}
         />
         <button>Submit</button>
